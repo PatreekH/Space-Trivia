b5cec3fb487a5bd2029fc0032581285c242808bd
Patrick Hernandez <r.patrick.hernandez@hotmail.com>
2016-05-02 17:27:07 -0400

    added answerCheck to JS logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,14 +12,15 @@ $(document).ready(function() {
 		var textFade = function(){
 			$(".ansFade").fadeIn(1000);
 			$(".question").fadeIn(1000);
-			//$(".mainDiv").fadeIn(1000);
+			$(".timer").fadeIn(1000);
 		};
 
 		$(".ansFade").hide();
 		$(".question").hide();
-		//$(".mainDiv").hide();
+		$(".timer").hide();
 
-		//$(".mainDiv").animate({opacity: 1}, 1000);
+		//$(".mainDiv").animate({width: "100%", height: "100%"}, 1000);
+		//$(".mainDiv").css("background-color", "white");
 		$(".allAns").animate({width: "100%", height: "100%"}, 1000);
 		$(".allAns").css("background-color", "white");
 
@@ -29,6 +30,7 @@ $(document).ready(function() {
    		$(".bodiv").addClass("invsDiv");
    		$(".startText").addClass("invsDiv");
    		$(".question").removeClass("invsDiv");
+   		$(".timer").removeClass("invsDiv");
 
    		Startgame();
 
@@ -36,22 +38,32 @@ $(document).ready(function() {
 
 	var Startgame = function(){
 
-	var q1 = "This is question 1";
-	var q1a1 = "This is Answer 1";
-	var q1a2 = "This is Answer 2";
-	var q1a3 = "This is Answer 3";
-	var q1a4 = "This is Answer 4";
-	var q2 = "this is question 2";
-	var q2a1 = "This is Answer 1";
-	var q2a2 = "This is Answer 2";
-	var q2a3 = "This is Answer 3";
-	var q2a4 = "This is Answer 4";
+		var q1 = "This is question 1";
+		var q1a1 = "This is Answer 1";
+		var q1a2 = "This is Answer 2";
+		var q1a3 = "This is Answer 3";
+		var q1a4 = "This is Answer 4";
+		var userguess1;
+		var q2 = "this is question 2";
+		var q2a1 = "This is Answer 1";
+		var q2a2 = "This is Answer 2";
+		var q2a3 = "This is Answer 3";
+		var q2a4 = "This is Answer 4";
 
-	var question1 = new question(q1, q1a1, q1a2, q1a3, q1a4);//correct answer goes on the end here//userguess1
+		//put new question in an array and call it after win/lose page timer is up
+
+
+		var question1 = new question(q1, q1a1, q1a2, q1a3, q1a4, "#ans1");//correct answer goes on the end here//userguess1
+
+			if (userguess1 == true){
+				//add timer function
+				var question2 = new question(q2, q2a1, q2a2, q2a3, q2a4, q2a3);
+			}
+
 		//if userguess1 == true after blah seconds transfer to next question
 	}
 
-	var question = function(q, a1, a2, a3, a4, correct, userGuessNum){
+	var question = function(q, a1, a2, a3, a4, correct){
 
 		this.q = q;
 		this.a1 = a1;
@@ -59,7 +71,31 @@ $(document).ready(function() {
 		this.a3 = a3;
 		this.a4 = a4;
 		this.correct = correct;
-		this.userGuessNum = userGuessNum;
+		var correctguess;
+		secondsLeft = 32;
+
+		var qTimeDecrement = function(){
+			interv = setInterval(qTimer, 1000);
+		}
+
+		var qTimer = function(){
+			if (secondsLeft > 0){
+				secondsLeft--;
+				$(".timeLeft").html(secondsLeft);
+			}
+			else if (secondsLeft == 0){
+				qTimeUp();
+			}
+		}
+
+		var qTimeUp = function(){
+			$(".question").addClass("invsDiv");
+   			$(".timer").addClass("invsDiv");
+   			$(".timeOut").removeClass("invsDiv");
+		}
+
+		qTimeDecrement();
+		qTimer();
 
 		$(".question").html(q);
 		$("#ans1").html(a1);
@@ -67,13 +103,34 @@ $(document).ready(function() {
 		$("#ans3").html(a3);
 		$("#ans4").html(a4);
 
-		//.on click correct
-		//correctguess = true
+		$(correct).removeClass("otherAns");
 
-		//.on click allAns
-		//if correctguess == true{ userguess = true, navigate to correct.} else { userguess = true highlight word in red and show incorrect page}
+		$(correct).on("click", function(){
+			correctguess = true;
+			checkAnswer();
+		});
 
-	}
+		$(".otherAns").on("click", function(){
+			correctguess = false;
+			checkAnswer();
+		});
+
+		var checkAnswer = function(){
+			if (correctguess == true){
+				$(".question").addClass("invsDiv");
+   				$(".timer").addClass("invsDiv");
+   				$(".correctGuess").removeClass("invsDiv");
+   			}
+   			else {
+				$(".question").addClass("invsDiv");
+   				$(".timer").addClass("invsDiv");
+   				$(".incorrectGuess").removeClass("invsDiv")
+   			}
+		};
+
+		//if userguess = true, inverval 5 seconds, then call for loop that runs thru questions? or function that holds questions
+
+	};
 
 
 });
